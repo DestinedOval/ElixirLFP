6b87de51b34aab01961bdbdccd987c742df3c494
Stew <stewartwilson1389@gmail.com>
2024-07-08 20:18:36 +0100

    Added room for resting/healing

---
+++ lib/dungeon_crawl/room/triggers/rest.ex
@@ -0,0 +1,19 @@
+defmodule DungeonCrawl.Room.Triggers.Rest do
+  @behaviour DungeonCrawl.Room.Trigger
+
+  alias DungeonCrawl.Character
+  alias Mix.Shell.IO, as: Shell
+
+  def run(character, %DungeonCrawl.Room.Action{id: :forward}) do
+    {character, :forward}
+  end
+
+  def run(character, %DungeonCrawl.Room.Action{id: :rest}) do
+    healing_value = Enum.random(3..6)
+    updated_char = Character.heal(character, healing_value)
+    Shell.info("You take a moment to catch your breath and bandage your wounds.")
+    Shell.info("You heal for #{healing_value} HP.")
+    Shell.info("You now have #{updated_char.hit_points} / #{updated_char.max_hit_points} HP.")
+    {updated_char, :forward}
+  end
+end
